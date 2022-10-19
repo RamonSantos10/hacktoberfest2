e2552463fee6bb0767286e1f697c2f16195b2c96
RamonSantos10 <90793910+RamonSantos10@users.noreply.github.com>
2022-10-19 11:47:32 -0300

    Add files via upload

---
+++ Exercicio_1/AMS_Tipos_Idade22/Program.cs
@@ -0,0 +1,12 @@
+﻿int idadeMin = 0;
+int idadeMax = 0;
+int idadeMed = 0;
+
+Console.Write("Digite a Idade mínima: ");
+idadeMin = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite a Idade máxima: ");
+idadeMax = Convert.ToInt32(Console.ReadLine());
+
+idadeMed = ((idadeMin + idadeMax) / 2);
+Console.WriteLine($"Idade média: {idadeMed}");
